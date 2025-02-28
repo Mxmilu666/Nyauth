67a917a0260742af4c700f9a3ef82f45c3faa6c7
Mxmilu666 <mxmilu666@gmail.com>
2025-02-28 23:31:03 +0800

    feat(frontend): update to use ts

--- frontend/src/pages/Userauth/Otpform.vue
+++ frontend/src/pages/Userauth/Otpform.vue
@@ -1,10 +1,11 @@
-<script setup>
-import { ref } from 'vue'
-const otp = ref('')
+<script setup lang="ts">
+import { defineOptions, ref } from 'vue'
 
 defineOptions({
     name: 'Otpform'
 })
+
+const otp = ref('')
 
 const props = defineProps({
     email: {
@@ -21,7 +22,7 @@ const props = defineProps({
 <template>
     <div>
         <v-sheet color="surface">
-            <v-otp-input v-model="otp" type="" />
+            <v-otp-input v-model="otp" type="text" />
         </v-sheet>
         <p class="text-center">验证码已发送至 {{ email }}</p>
         <p class="text-center">请注意查收并验证</p>
