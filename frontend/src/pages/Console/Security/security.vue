ebf8d9a8f0332b30e94c7d121cce56a7b7795bb6
Mxmilu666 <mxmilu666@gmail.com>
2025-03-23 15:00:05 +0800

    refactor(frontend): SecurityPage

--- frontend/src/pages/Console/Security/security.vue
+++ frontend/src/pages/Console/Security/security.vue
@@ -1,28 +1,11 @@
-<template>
-    <div>
-        <!-- 先拿来占用一下测试个验证码晚点再还回去( -->
-        <Turnstile
-            v-model:show="showTurnstile"
-            @callback="handleCaptchaVerify"
-            @error="handleCaptchaError"
-        />
+<script setup lang="ts">
+import { defineOptions } from 'vue'
 
-        <v-btn @click="showTurnstile = true">进行人机验证</v-btn>
-    </div>
-</template>
-
-<script setup>
-import { ref } from 'vue'
-import Turnstile from '@/components/turnstile/Turnstile.vue'
-
-const showTurnstile = ref(false)
-
-const handleCaptchaVerify = (token) => {
-    console.log('验证成功，获取到token:', token)
-
-}
-
-const handleCaptchaError = (error) => {
-    console.error('验证码错误:', error)
-}
+defineOptions({
+    name: 'SecurityPage'
+})
 </script>
+
+<template>
+    <p>Helloworld</p>
+</template>
